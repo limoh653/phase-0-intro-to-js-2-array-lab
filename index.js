893609dec4049c64cb79a114d907f263b3cbc95d
limoh653 <limohesbon7@gmail.com>
2024-06-27 17:56:13 +0300

    objects

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+const cats= ["Milo", "Otis", "Garfield"]
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+ function  destructivelyAppendCat(Ralph){
+    cats.push(Ralph)
+ }
+ function destructivelyPrependCat(Bob){
+    cats.unshift(Bob)
+ }
+function destructivelyRemoveLastCat(Garfield){
+    cats.pop(Garfield)
+}
+  function destructivelyRemoveFirstCat(Milo){
+    cats.shift(Milo)
+  }
+  function appendCat(Broom){
+    const modified=[...cats,"Broom"]
+    return modified;
+  }
+  function prependCat(Arnold){
+    const prepend=["Arnold",...cats]
+    return prepend;
+
+  }
+function removeLastCat(){
+    return cats.slice(0,2);
+}
+function removeFirstCat(){
+   return cats.slice(1);
+
+}
